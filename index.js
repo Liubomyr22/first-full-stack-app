948d6222fcc46bfa08719da91bff4ca987e894d6
Liubomyr Kraichyk <liubomyr.kraichyk@pettersonapps.com>
2021-07-27 12:29:07 +0300

    changed port

--- index.js
+++ index.js
@@ -42,6 +42,6 @@ app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
 
-app.listen(PORT, () => {
+app.listen("4000", () => {
   console.log("Backend is running.");
 });
